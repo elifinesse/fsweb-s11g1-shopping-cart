b543fdddc1af30a10d5fdb8e4d0dd308dd48b11b
elifinesse <133097200+elifinesse@users.noreply.github.com>
2023-08-07 12:14:58 +0300

    replaced props with CartContext + ProductContext

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { ProductContext } from "./contexts/ProductContext";
 import Navigation from "./components/Navigation";
 import Products from "./components/Products";
 import ShoppingCart from "./components/ShoppingCart";
+import { CartContext } from "./contexts/CartContext";
 
 function App() {
   const [products, setProducts] = useState(data);
@@ -20,18 +21,20 @@ function App() {
   return (
     <div className="App">
       <ProductContext.Provider value={{ products, addItem }}>
-        <Navigation cart={cart} />
+        <CartContext.Provider value={{ cart }}>
+          <Navigation />
 
-        {/* Routelar */}
-        <main className="content">
-          <Route exact path="/">
-            <Products products={products} addItem={addItem} />
-          </Route>
+          {/* Routelar */}
+          <main className="content">
+            <Route exact path="/">
+              <Products />
+            </Route>
 
-          <Route path="/cart">
-            <ShoppingCart cart={cart} />
-          </Route>
-        </main>
+            <Route path="/cart">
+              <ShoppingCart />
+            </Route>
+          </main>
+        </CartContext.Provider>
       </ProductContext.Provider>
     </div>
   );
