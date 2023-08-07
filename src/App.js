f89f3950a6e00a6a1c03d065f4b854d23bff191e
elifinesse <133097200+elifinesse@users.noreply.github.com>
2023-08-07 12:35:54 +0300

    local storage update with addItem and removeItem

--- src/App.js
+++ src/App.js
@@ -2,23 +2,31 @@ import React, { useState } from "react";
 import { Route } from "react-router-dom";
 import { data } from "./data";
 import { ProductContext } from "./contexts/ProductContext";
+import { CartContext } from "./contexts/CartContext";
 
 // Bileşenler
 import Navigation from "./components/Navigation";
 import Products from "./components/Products";
 import ShoppingCart from "./components/ShoppingCart";
-import { CartContext } from "./contexts/CartContext";
 
 function App() {
   const [products, setProducts] = useState(data);
-  const [cart, setCart] = useState([]);
+  const lsCart =
+    localStorage.getItem("cart") === null
+      ? []
+      : JSON.parse(localStorage.getItem("cart"));
+  const [cart, setCart] = useState(lsCart);
 
   const addItem = (item) => {
     // verilen itemi sepete ekleyin
-    setCart([...cart, item]);
+    const newCart = [...cart, item];
+    setCart(newCart);
+    localStorage.setItem("cart", JSON.stringify(newCart));
   };
   const removeItem = (id) => {
-    setCart(cart.filter((item) => id !== item.id));
+    const newCart = cart.filter((item) => id !== item.id);
+    setCart(newCart);
+    localStorage.setItem("cart", JSON.stringify(newCart));
   };
   return (
     <div className="App">
